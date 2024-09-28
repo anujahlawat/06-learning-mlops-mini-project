79ad99b8e72e60bf6bdd0b864414d82789ab91b0
Anuj Ahlawat <anujahlawat0466@gmail.com>
2024-09-29 00:32:48 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a small mlops project and basically it is related to ml part of ml+ops',
+    author='anuj',
+    license='',
+)
